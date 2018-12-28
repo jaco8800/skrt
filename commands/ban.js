29ebeb8dee018184cf08a839500f8e9d2b2c5462
Trace <46228022+TraceLD@users.noreply.github.com>
2018-12-28 21:18:34 +0100

    Minor fixes

--- commands/ban.js
+++ commands/ban.js
@@ -6,23 +6,16 @@ module.exports.run = async (bot, message, args) => {
   //${botconfig.prefix}ban @jack annoying
   const bUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
 
-  if (!bUser) {
-    return message.channel.send("User does not exist.");
-  }
+  if (!bUser) return message.channel.send("User does not exist.");
+
   const bReason = args.join(" ").slice(22);
 
-  if (!message.member.hasPermission("BAN_MEMBERS")) {
-    return message.channel.send("No permission.");
-  }
-
-  if (bUser.hasPermission("BAN_MEMBERS")) {
-    return message.channel.send("You can't kick a moderator.");
-  }
-
-  if (!bReason) {
-    return message.channel.send("Give reason");
-  }
-
+  if (!message.member.hasPermission("BAN_MEMBERS")) return message.channel.send("No permission.");
+    
+  if (bUser.hasPermission("BAN_MEMBERS")) return message.channel.send("You can't kick a moderator.");
+    
+  if (!bReason) return message.channel.send("Give reason");
+ 
   const banEmbed = new Discord.RichEmbed()
     .setDescription("¡¡¡  Ban  !!!")
     .setColor(botconfig.red)
@@ -32,9 +25,7 @@ module.exports.run = async (bot, message, args) => {
     .addField("Reason", bReason);
 
   message.guild.member(bUser).ban(bReason);
-
   message.channel.send({ embed: banEmbed });
-
 };
 
 module.exports.help = {
